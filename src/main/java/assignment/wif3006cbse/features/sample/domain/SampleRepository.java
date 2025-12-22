11bbecb4ca10a7fbe23904ac3f7c106105163b60
fyiernzy <fyiernzy@gmail.com>
2025-12-22 13:48:50 +0800

    feat: Remove Gradle wrapper and build configuration files.

--- src/main/java/assignment/wif3006cbse/features/sample/domain/SampleRepository.java
+++
@@ -1,9 +0,0 @@
-package assignment.wif3006cbse.features.sample.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SampleRepository extends JpaRepository<Sample, Long> {
-
-}
--- src/main/java/assignment/wif3006cbse/features/sample/domain/SampleStatusEnum.java
+++
@@ -1,4 +0,0 @@
-package assignment.wif3006cbse.features.sample.domain;
-
-public enum SampleStatusEnum {
-}
