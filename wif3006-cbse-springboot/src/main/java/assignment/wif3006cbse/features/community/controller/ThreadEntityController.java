a1658136286d46c5a4d10758dab2fc98c2fbfa8b
fyiernzy <fyiernzy@gmail.com>
2026-01-12 14:39:56 +0800

    style(community): rename ThreadController.java to ThreadEntityController.java

--- wif3006-cbse-springboot/src/main/java/assignment/wif3006cbse/features/community/controller/ThreadEntityController.java
+++ wif3006-cbse-springboot/src/main/java/assignment/wif3006cbse/features/community/controller/ThreadEntityController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/threads")
-public class ThreadController {
+public class ThreadEntityController {
 
     private final ThreadEntityService threadEntityService;
 
