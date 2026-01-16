6855898af6aaaf1ebc83c6c9ebcb6d1c17b6ffd7
fyiernzy <fyiernzy@gmail.com>
2026-01-16 16:03:05 +0800

    Refactor to OSGi-style bundles

--- wif3006-cbse-osgi/src/main/java/assignment/wif3006cbse/shared/pagination/Pageable.java
+++
@@ -1,8 +0,0 @@
-package assignment.wif3006cbse.shared.pagination;
-
-public record Pageable(int page, int size) {
-
-    public static Pageable of(int page, int size) {
-        return new Pageable(page, size);
-    }
-}
